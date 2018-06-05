1ca4ca2d5885b0935d4eecf92bb4d0287b3704cc
damartinezru <danielamr15@hotmail.com>
2018-06-05 13:51:31 -0500

    casi sirviendo

---
+++ src/perseguido/cliente/ControlCliente.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perseguido.cliente;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import perseguido.gui.Lienzo;
+import perseguido.gui.Marcador;
+import perseguido.gui.Principal;
+import perseguido.logica.Heroe;
+import perseguido.servidor.HiloCliente;
+/**
+ *
+ * @author chamo
+ */
+public class ControlCliente implements KeyListener{
+    public DataInputStream dataInput;
+    public DataOutputStream dataOutput;
+    Lienzo lienzo;
+    Marcador marcador= new Marcador();
+        
+    //Heroe h= new Heroe();
+
+    public void setLienzo(Lienzo lienzo) {
+        this.lienzo = lienzo;
+        lienzo.setDataInput(dataInput);
+        lienzo.setDataOutput(dataOutput);
+    }
+    
+    
+    
+    public ControlCliente(Socket socket)
+    {
+        
+        marcador.setVisible(true);
+        try
+        {
+            dataInput = new DataInputStream(socket.getInputStream());
+            dataOutput = new DataOutputStream(socket.getOutputStream());
+            
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        lienzo.getHeroe().update(e.getKeyChar());
+        lienzo.getDataOutput();
+        marcador.cuadro.setText(lienzo.getPuntos());
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        
+
+           
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        
+    }
+    
+     public static void main(String[] args)
+    {
+        
+    }
+}
---
+++ src/perseguido/cliente/Datos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perseguido.cliente;
+
+/**
+ *
+ * @author GOLDS
+ */
+public class Datos {
+    
+    public void recibir(String x){
+    
+        System.out.println(x);
+    }
+}
